ef472058b06140eeeeb394dd4f02ae967f834f4a
Zehra Sena Akgül <74866817+zehrasenakgul@users.noreply.github.com>
2022-05-31 01:58:08 +0300

    update

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -5,13 +5,26 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Models\Skill;
 use App\Models\Work;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
     public function home()
     {
-        $works = Work::limit(3)->get();
-        $skills = Skill::all();
+        if (Cache::has("skills")) {
+            $skills = Cache::get("skills");
+        } else {
+            $skills = Skill::all();
+            Cache::put("skills", $skills, 5);
+        }
+        if (Cache::has("works")) {
+
+            $works = Cache::get("works");
+        } else {
+            $works = Work::limit(3)->get();
+            Cache::put("works", $works, 5);
+        }
+
         return view("frontend.home.index", compact("works", "skills"));
     }
 
